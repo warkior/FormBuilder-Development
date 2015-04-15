9aa10e785932cd2c0764999fcd960d8292aa0b09
James Warkentin <james@truthmedia.com>
2015-04-15 09:49:06 -0700

    Minor bugfix for 1.06

--- php/formbuilder_admin_pages.inc.php
+++ php/formbuilder_admin_pages.inc.php
@@ -718,11 +718,11 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  			{
  				die("No form fields found.");
  			}
- 			
- 			unset($autoresponse['id']);
+
  			if($autoresponse)
  			{
  			formbuilder_debug("Creating autoresponse: '{$autoresponse['name']}'");
+			    unset($autoresponse['id']);
  			$result = $wpdb->insert(FORMBUILDER_TABLE_RESPONSES, $autoresponse);
  			if($result != 1)
  			{
